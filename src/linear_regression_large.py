40967f029c98a89169544d394af9f2b914351c10
Chen Liang <chen.liang@ie.ibm.com>
2024-02-17 17:59:30 +0000

    large data file

---
+++ src/linear_regression_large.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import pandas
+from sklearn import linear_model
+
+df = pandas.read_csv("data_files/winequality-red.csv")
+
+X = df[["fixed acidity",
+        "volatile acidity",
+        "citric acid",
+        "residual sugar",
+        "chlorides",
+        "free sulfur dioxide",
+        "total sulfur dioxide",
+        "density",
+        "pH",
+        "sulphates",
+        "alcohol"]]
+y = df["quality"]
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+print(regr.coef_)
